082c2fc310c1d4bd996faa08d8ed1724d1347df1
oltgvshi <oltgvshi@gmail.com>
2021-12-26 22:55:28 +0100

    Validimi LOGIN!!!
    Co-Authored-By: Luaniiii <93317143+Luaniiii@users.noreply.github.com>

---
+++ login.js
@@ -0,0 +1,37 @@
+var button = document.getElementById("loginbuttoni");
+
+var usernameMsg = document.getElementById("usernameerrorlogin");
+var passwordMsg = document.getElementById("passworderrorlogin");
+var usernameRegex = /[a-z]+[.]+[a-z]+/;
+var passwordRegex = /^[A-Z]\w+[a-z]\d{3}/;
+
+button.addEventListener("click", function(event){
+    var username = document.getElementById("usernameinputlogin").value;
+    var password = document.getElementById("passwordinputlogin").value;
+  
+    if(username == "" || username == null){
+      usernameMsg.innerText="Please fill the username field!";
+      event.preventDefault();
+    }else{
+      if(usernameRegex.test(username)){
+          usernameMsg.innerText="";
+      }else{
+          usernameMsg.innerText="* Username must be example.example";
+          event.preventDefault();
+      }
+  
+  }
+  
+    if (password == "" || password == null) {
+      passwordMsg.innerText = "* Please fill the password field";
+      event.preventDefault();
+    } else {
+      if (passwordRegex.test(password)) {
+        passwordMsg.innerText = "";
+      } else {
+        passwordMsg.innerText =
+          "* Password should start with a capital and end with 3 numbers";
+        event.preventDefault();
+      }
+    }
+  });
